ad82c193ec13fb9e828948d7b8d7e3df1b5d5dc6
PinkDeer <bunman@mail.ru>
2018-05-15 21:11:10 +0300

    Added animation

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,25 @@
   <div class="training">
     <h1>Math training</h1>
     <hr>
-    <app-start-screen
-      v-if="state == 'start'"
-      @onStart="onStart">
-    </app-start-screen>
-    <app-question v-else-if="state == 'question'"
-      @success="onQuestSuccess"
-      @error="onQuestError"
-    ></app-question>
-    <app-message v-else-if="state == 'message'"
-      :type="message.type"
-      :text="message.text"
-      @onNext="onNext">
-    </app-message>
-    <app-resault-screen v-else-if="state == 'resaults'"></app-resault-screen>
-    <div v-else>Unknown state</div>
+    <div calss="box">
+      <transition name="flip" mode="out-in">
+        <app-start-screen
+          v-if="state == 'start'"
+          @onStart="onStart">
+        </app-start-screen>
+        <app-question v-else-if="state == 'question'"
+          @success="onQuestSuccess"
+          @error="onQuestError"
+        ></app-question>
+        <app-message v-else-if="state == 'message'"
+          :type="message.type"
+          :text="message.text"
+          @onNext="onNext">
+        </app-message>
+        <app-resault-screen v-else-if="state == 'resaults'"></app-resault-screen>
+        <div v-else>Unknown state</div>
+      </transition>
+    </div>
   </div>
 </template>
 
@@ -25,10 +29,19 @@ export default {
   data () {
     return {
       state: 'start',
+      stats: {
+        succeses: 0,
+        error: 0
+      },
       message: {
         type: '',
         text: ''
       }
+    }
+  },
+  computed: {
+    questDone(){
+      return this.stats.success + this.stats.error;
     }
   },
   methods: {
@@ -57,4 +70,27 @@ export default {
     max-width: 700px;
     margin: 20px auto;
   }
+
+  .flip-enter {
+
+  }
+  .flip-enter-active {
+    animation: flipInX 0.3s linear;
+  }
+  .flip-leave {
+
+  }
+  .flip-leave-active {
+    animation: flipOutX 0.3s linear;
+  }
+
+  @keyframes flipInX {
+    from{transform: rotateX(90deg);}
+    to{transform: rotateX(0deg);}
+  }
+  @keyframes flipOutX {
+    from{transform: rotateX(0deg);}
+    to{transform: rotateX(90deg);}
+  }
+
 </style>
