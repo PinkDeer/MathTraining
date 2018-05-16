5ec563f9afbaa94660f56542dfd545f8ec664de5
PinkDeer <bunman@mail.ru>
2018-05-16 15:10:15 +0300

    Added progress bar

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
   <div class="training">
     <h1>Math training</h1>
     <hr>
-    <div calss="box">
+    <div class="progress">
+      <div class="progress-bar" :style="progressStyles"></div>
+    </div>
+    <div class="box">
       <transition name="flip" mode="out-in">
         <app-start-screen
           v-if="state == 'start'"
@@ -30,18 +33,24 @@ export default {
     return {
       state: 'start',
       stats: {
-        succeses: 0,
+        success: 0,
         error: 0
       },
       message: {
         type: '',
         text: ''
-      }
+      },
+      questMax: 3
     }
   },
   computed: {
     questDone(){
       return this.stats.success + this.stats.error;
+    },
+    progressStyles(){
+      return {
+        width: (this.questDone / this.questMax * 100) + '%'
+      };
     }
   },
   methods: {
@@ -50,13 +59,15 @@ export default {
     },
     onQuestSuccess(){
       this.state = 'message';
-      this.message.text = 'Good Job!'
-      this.message.type = 'success'
+      this.message.text = 'Good Job!';
+      this.message.type = 'success';
+      this.stats.success++;
     },
     onQuestError(msg){
       this.state = 'message';
       this.message.text = msg;
-      this.message.type = 'warning'
+      this.message.type = 'warning';
+      this.stats.error++;
     },
     onNext(){
       this.state = 'question';
@@ -69,6 +80,10 @@ export default {
   .training {
     max-width: 700px;
     margin: 20px auto;
+  }
+
+  .box {
+    margin: 10px 0;
   }
 
   .flip-enter {
